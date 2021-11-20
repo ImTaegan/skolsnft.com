243df4de5aff9493d4b2777995b880b26b891021
Lukasd <lukasd@hotmail.cz>
2021-11-20 11:52:00 +0100

    v3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="grid grid-custom md:items-center lg:auto-cols-max lg:grid-flow-col mx-8 my-4 md:mx-12 md:my-6 lg:mx-20 lg:my-10">
+    <nav className="grid grid-custom md:items-center lg:auto-cols-max lg:grid-flow-col max-w-screen-xl mx-8 my-4 md:mx-12 md:my-6 lg:mx-20 lg:my-10 xl:mx-auto">
       <Link href="/">
         <a className="hover:no-underline text-black text-2xl font-bold">
           SKOLs
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    safelist: ["grid-custom"],
-  },
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
